5c7369dace9255a074f74ba5eb368b3ca8af27a3
michal <michal131711@gmail.com>
2023-12-18 12:56:00 +0100

    create logger and try catch main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {MyLogger} from "./config/logger";
+import {Logger} from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule, {
+      logger: new MyLogger()
+    });
+    
+    await app.listen(3000);
+  }catch (e) {
+    Logger.error(`❌  Error starting server, ${e}`, '', 'Bootstrap')
+    process.exit()
+  }
+
 }
-bootstrap();
+bootstrap().catch(e => {
+  Logger.error(`❌  Error starting server, ${e}`, '', 'Bootstrap')
+  throw e
+});
