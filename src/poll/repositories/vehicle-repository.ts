70b8792dbac497003669ddac97ab488e9a597fb7
michal <michal131711@gmail.com>
2023-12-23 17:34:37 +0100

    add vehicle repository

---
+++ src/poll/repositories/vehicle-repository.ts
@@ -0,0 +1,9 @@
+import {EntityRepository, Repository} from "typeorm";
+import {VehicleEntity} from "@entities";
+
+@EntityRepository(VehicleEntity)
+export class VehicleRepository extends Repository<VehicleEntity> {
+	async findByName(name: string): Promise<VehicleEntity> {
+		return this.findOneBy({name});
+	}
+}
