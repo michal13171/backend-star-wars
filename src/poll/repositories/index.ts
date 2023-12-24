25cd49ca92ee1ec66a390f205202a26bd7f75bc4
michal <michal131711@gmail.com>
2023-12-24 10:08:00 +0100

    optimize import repositories

---
+++ src/poll/repositories/index.ts
@@ -0,0 +1,6 @@
+export * from './starship-repository';
+export * from './vehicle-repository';
+export * from './species-repository';
+export * from './planet-repository';
+export * from './people-repository';
+export * from './film-repository';
