5afda1afac9ea2f425c143c57ff417dede18da7f
michal <michal131711@gmail.com>
2023-12-24 11:17:49 +0100

    clean servies app and controller

--- src/poll/controllers/app.controller.ts
+++ src/poll/controllers/app.controller.ts
@@ -1,15 +1,9 @@
-import {Controller, Get, UseInterceptors} from '@nestjs/common';
-import { AppService } from '../services/app.service';
-import {CacheInterceptor} from "@nestjs/cache-manager";
-import {PeopleInterface} from "../interfaces/people.interface";
+import {Controller, Get} from '@nestjs/common';
 
 @Controller()
-@UseInterceptors(CacheInterceptor)
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-	
-  @Get()
-  getPeople(): Promise<PeopleInterface[]> {
-    return this.appService.getPeoples();
-  }
+	@Get()
+	helloworld(): string {
+		return "Hello World";
+	}
 }
