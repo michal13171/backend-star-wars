df9c40ccea18eb51ab4de001caf9c48c5b1ebb96
michal <michal131711@gmail.com>
2023-12-25 19:54:20 +0100

    swagger people.controller.ts

--- src/poll/controllers/people.controller.ts
+++ src/poll/controllers/people.controller.ts
@@ -3,24 +3,38 @@ import {PeopleService} from "@services";
 import {PeopleEntity} from "@entities";
 import {PaginationDto} from "../dto/pagination.dto";
 import {CacheInterceptor} from "@nestjs/cache-manager";
-import {CacheKey} from "@nestjs/common/cache";
-import {ApiResponse, ApiTags} from "@nestjs/swagger";
+import {ApiQuery, ApiResponse, ApiTags} from "@nestjs/swagger";
 
-@Controller('peoples')
+@UseInterceptors(CacheInterceptor)
 @ApiTags('peoples')
+@Controller('peoples')
 export class PeopleController {
 	constructor(private peopleService: PeopleService) {}
 	
-	@UseInterceptors(CacheInterceptor)
-	@CacheKey("people")
 	@Get()
-	@ApiResponse({ status: 200, description: `Get all people with pagination example: ?page=1&pageSize=5` })
+	@ApiQuery({
+		name: 'page',
+		required: true,
+		type: Number,
+	})
+	@ApiQuery({
+		name: 'pageSize',
+		required: true,
+		type: Number,
+	})
+	@ApiResponse({
+		status: 200,
+		description: `Get all people with pagination example: ?page=1&pageSize=5`
+	})
 	getAllPeople(@Query() paginationDto: PaginationDto): Promise<PeopleEntity[]> {
 		return this.peopleService.getAllPeople(paginationDto);
 	}
 	
 	@Get(':id')
-	@ApiResponse({ status: 200, description: `Single people with relationships`})
+	@ApiResponse({
+		status: 200,
+		description: `Single people with relationships`
+	})
 	getPeople(@Param('id', ParseIntPipe) id: number): Promise<PeopleEntity> {
 		return this.peopleService.getPeople(id);
 	}
