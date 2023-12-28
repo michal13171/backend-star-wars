ab2250f9f0adff19adafc5f4f2ac86c406a32135
michal <michal131711@gmail.com>
2023-12-28 12:08:44 +0100

    correct desc films and add port redis default

--- src/poll/controllers/film.controller.ts
+++ src/poll/controllers/film.controller.ts
@@ -1,41 +1,36 @@
-import {Controller, Get, Param, ParseIntPipe, Query, UseInterceptors} from '@nestjs/common';
-import {FilmService} from "@services";
-import {CacheInterceptor} from "@nestjs/cache-manager";
-import {PaginationDto} from "../dto/pagination.dto";
-import {FilmEntity} from "@entities";
-import {ApiQuery, ApiResponse, ApiTags} from "@nestjs/swagger";
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilmService } from '@services';
+import { CacheInterceptor } from '@nestjs/cache-manager';
+import { FilmEntity } from '@entities';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('films')
 @UseInterceptors(CacheInterceptor)
 @Controller('films')
 export class FilmController {
-	constructor(private filmService: FilmService) {}
-	
-	@Get()
-	@ApiQuery({
-		name: 'page',
-		required: true,
-		type: Number,
-	})
-	@ApiQuery({
-		name: 'pageSize',
-		required: true,
-		type: Number,
-	})
-	@ApiResponse({
-		status: 200,
-		description: `Get all films with pagination example: ?page=1&pageSize=5 in url address you will see unique words with counted length`
-	})
-	getAllPeople(@Query() paginationDto: PaginationDto): any {
-		return this.filmService.getAllFilms(paginationDto);
-	}
-	
-	@Get(':id')
-	@ApiResponse({
-		status: 200,
-		description: `Single film with relationships`
-	})
-	getPeople(@Param('id', ParseIntPipe) id: number): Promise<FilmEntity> {
-		return this.filmService.getFilm(id);
-	}
+  constructor(private filmService: FilmService) {}
+
+  @Get()
+  @ApiResponse({
+    status: 200,
+    description: `Get all unique words with counted length`,
+  })
+  getAllPeople(): any {
+    return this.filmService.getAllFilms();
+  }
+
+  @Get(':id')
+  @ApiResponse({
+    status: 200,
+    description: `Single film with relationships`,
+  })
+  getPeople(@Param('id', ParseIntPipe) id: number): Promise<FilmEntity> {
+    return this.filmService.getFilm(id);
+  }
 }
