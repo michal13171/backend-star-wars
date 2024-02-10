e768eea20c48cf41538ec0cedbc6e877fd0936dc
michal <michal131711@gmail.com>
2024-02-10 17:04:11 +0100

    change env

--- src/config/environments/index.ts
+++ src/config/environments/index.ts
@@ -1,9 +1,9 @@
-const NODE_ENV: string = process.env.NODE_ENV || 'development';
+const NODE_ENV: string = process.env.NODE_ENV ?? 'development';
 const REDIS = {
-  host: process.env.REDIS_HOST || 'localhost',
-  port: parseInt(process.env.REDIS_PORT) || 6379,
+  host: process.env.REDIS_HOST ?? 'nestjs_redis',
+  port: 6379,
 };
-const UL_SWAPI: string = 'https://swapi.dev/api/';
+const UL_SWAPI: string = 'https://swapi.dev/api';
 const PRIMARY_COLOR: string = '#87e8de';
 const PORT: number = 3000;
 const RATE_LIMIT_MAX: number = 10000;
