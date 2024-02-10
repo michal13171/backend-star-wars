4a2029d7215655bb5d73786adbb9f680b0d09f95
michal <michal131711@gmail.com>
2024-02-10 17:39:44 +0100

    back cache

--- src/poll/people/people.module.ts
+++ src/poll/people/people.module.ts
@@ -1,20 +1,26 @@
 import { Module } from '@nestjs/common';
 import { PeopleController } from './controllers/people.controller';
-import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
-import { RedisOptions } from '../../config/cache';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { PeopleService } from './services/people.service';
+import { AppService } from '../../config/services/app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { REDIS } from '@environments';
+import { CacheModule } from '@nestjs/common/cache';
+import { RedisOptions } from '../../config/cache';
 
 @Module({
-  imports: [CacheModule.registerAsync(RedisOptions), PeopleModule],
-  controllers: [PeopleController],
-  providers: [
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CacheInterceptor,
-    },
-    PeopleService,
+  imports: [
+    CacheModule.registerAsync(RedisOptions),
+    ClientsModule.register([
+      {
+        name: 'GLOBAL_PEOPLE_SERVICE',
+        transport: Transport.REDIS,
+        options: REDIS,
+      },
+    ]),
+    PeopleModule,
   ],
+  controllers: [PeopleController],
+  providers: [AppService, PeopleService],
   exports: [PeopleModule],
 })
 export class PeopleModule {}
