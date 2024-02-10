f844722f56426f4d5381c18125ce271dc9eb2213
michal <michal131711@gmail.com>
2024-02-10 17:15:52 +0100

    app.controller.ts change path

--- src/poll/controllers/app.controller.ts
+++ src/poll/controllers/app.controller.ts
@@ -1,12 +1,12 @@
-import {Controller, Get} from '@nestjs/common';
-import {ApiResponse, ApiTags} from "@nestjs/swagger";
+import { Controller, Get } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller()
 @ApiTags('hello')
 export class AppController {
-	@Get()
-	@ApiResponse({ status: 200, description: 'Hello world' })
-	helloWorld(): string {
-		return "Hello World";
-	}
+  @Get()
+  @ApiResponse({ status: 200, description: 'Hello world' })
+  helloWorld(): string {
+    return 'Hello World';
+  }
 }
