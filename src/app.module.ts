e20ff3dfa9935b1dcd215d8348e25d7596aa1fd5
michal <michal131711@gmail.com>
2024-02-11 14:17:17 +0100

    move app.controller.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UtilsModule } from './poll/utils.module';
+import { AppController } from './config/controllers/app.controller';
 
 @Module({
   imports: [UtilsModule],
+  controllers: [AppController],
 })
 export class AppModule {}
