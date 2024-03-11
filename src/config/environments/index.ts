bf89daf28782a31ef80d6eaf58cf3e23b3fe5362
michal <michal131711@gmail.com>
2024-03-11 16:58:51 +0100

    remove unused variables and change endpoint to exist

--- src/config/environments/index.ts
+++ src/config/environments/index.ts
@@ -1,11 +1,8 @@
-const NODE_ENV: string = process.env.NODE_ENV ?? 'development';
 const REDIS = {
   host: process.env.REDIS_HOST ?? 'localhost',
   port: 6379,
 };
-const UL_SWAPI: string = 'https://swapi.dev/api';
-const PRIMARY_COLOR: string = '#87e8de';
+const UL_SWAPI: string = 'https://swapi.py4e.com/api';
 const PORT: number = 3000;
-const RATE_LIMIT_MAX: number = 10000;
 
-export { NODE_ENV, REDIS, UL_SWAPI, PRIMARY_COLOR, PORT, RATE_LIMIT_MAX };
+export { REDIS, UL_SWAPI, PORT };
